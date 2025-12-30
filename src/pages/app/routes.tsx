dda5b4f7ed8716dd12f7eba72a448ee666eac42e
hafri23o <natasharockbell@gmail.com>
2025-12-30 08:43:37 +0200

    Update routes.tsx

--- src/pages/app/routes.tsx
+++ src/pages/app/routes.tsx
@@ -1,14 +1,14 @@
-import { RouteDefinition, Navigate } from 'solid-app-router'
+import { RouteDefinition, Navigate } from 'solid-app-router';
 import libraryRoute, {
   LIBRARY_PATH,
   DEFAULT_LIBRARY_PATH,
-} from '../library/route'
-import playerRoute from '../player/route'
-import detailsRoutes from '../details/route'
-import searchRoute from '../search/route'
-import settingsRoute from '../settings/route'
-import aboutRoute from '../about/route'
-import notFoundRoute from '../not-found/route'
+} from '../library/route';
+import playerRoute from '../player/route';
+import detailsRoutes from '../details/route';
+import searchRoute from '../search/route';
+import settingsRoute from '../settings/route';
+import aboutRoute from '../about/route';
+import notFoundRoute from '../not-found/route';
 
 export const ROUTES: RouteDefinition[] = [
   libraryRoute,
@@ -18,9 +18,14 @@ export const ROUTES: RouteDefinition[] = [
   settingsRoute,
   aboutRoute,
   {
+    // This will load index.html or any component you'd like as the default page
     path: '/',
-    children: [{ path: '/' }, { path: LIBRARY_PATH }],
+    element: () => <Navigate href={LIBRARY_PATH} />,
+  },
+  {
+    // Optionally, you can explicitly set the library or default route
+    path: LIBRARY_PATH,
     element: () => <Navigate href={DEFAULT_LIBRARY_PATH} />,
   },
   notFoundRoute,
-]
+];
