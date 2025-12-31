1604a6b62baf9c63c6f9bad9316640c6d89277e4
hafri23o <natasharockbell@gmail.com>
2025-12-31 07:27:58 +0200

    Update app.tsx

--- src/pages/app/app.tsx
+++ src/pages/app/app.tsx
@@ -1,11 +1,10 @@
 import { signOut } from 'firebase/auth'
 import { auth } from '../../firebase'
 
-export default function App() {
+export function App() {
   return (
     <div>
       <h1>App Dashboard</h1>
-      <button onClick={() => signOut(auth)}>Logout</button>
     </div>
   )
 }
