29c096f138b871d6562b2d271684d80d5cc373fd
hafri23o <natasharockbell@gmail.com>
2025-12-31 13:02:05 +0200

    Update routes.tsx

--- src/pages/app/routes.tsx
+++ src/pages/app/routes.tsx
@@ -1,15 +1,3 @@
-import { RouteDefinition, Navigate } from 'solid-app-router'
-import libraryRoute, {
-  LIBRARY_PATH,
-  DEFAULT_LIBRARY_PATH,
-} from '../library/route'
-import playerRoute from '../player/route'
-import detailsRoutes from '../details/route'
-import searchRoute from '../search/route'
-import settingsRoute from '../settings/route'
-import aboutRoute from '../about/route'
-import notFoundRoute from '../not-found/route'
-
 // src/pages/auth/route.tsx
 
 import { RouteDefinition, Navigate } from 'solid-app-router';
