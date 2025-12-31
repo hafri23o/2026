3f811da68361cb337339d17c3b000f434c38b4c3
hafri23o <natasharockbell@gmail.com>
2025-12-31 20:44:57 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,23 @@
-import { createApp } from 'solid-utils';
-import { Router, Route, Routes } from 'solid-app-router';
-import { MusicImagesProvider } from './components/music-image/data-context';
-import { MenuProvider } from './components/menu/menu';
-import { RootStoresProvider } from './stores/stores';
-import { ModalsProvider } from './components/modals/modals';
-import { ErrorPage } from './pages/error/error';
-import { App } from './pages/app/app';
-import LoginPage from './pages/auth/auth.tsx';
+import { Router } from 'solid-app-router'
+import { createApp } from 'solid-utils'
+import { MusicImagesProvider } from './components/music-image/data-context'
+import { MenuProvider } from './components/menu/menu'
+import { RootStoresProvider } from './stores/stores'
+import { ModalsProvider } from './components/modals/modals'
+import { ErrorPage } from './pages/error/error'
+import { App } from './pages/app/app'
 
-// Firebase initialization
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Check if user is logged in via Firebase
-const isLoggedIn = () => auth.currentUser !== null;
-
-const initialRender = () => {
-  // Dynamically use eval to decide which component to render based on login status
-  eval(`
-    if (isLoggedIn()) {
-      createApp(App)
-        .use(Router)
-        .use(MusicImagesProvider)
-        .use(RootStoresProvider)
-        .use(ModalsProvider)
-        .use(MenuProvider)
-        .mount('body');
-    } else {
-      createApp(() => (
-        <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/app" element={<App />} />
-          </Routes>
-        </Router>
-      ))
-        .use(RootStoresProvider)
-        .use(MusicImagesProvider)
-        .use(ModalsProvider)
-        .mount('body');
-    }
-  `);
-};
-
+// The supported browser features check file is very small,
+// still in case if it doesn't load or loads late
+// do not render app only if we explicitly know that browser is not supported.
+// If app loads in unsupported browser because of race condition it's not a big deal.
 if (window.isSupportedBrowser !== false) {
-  initialRender();
+  createApp(App)
+    .use(Router)
+    .use(ErrorPage)
+    .use(MusicImagesProvider)
+    .use(RootStoresProvider)
+    .use(ModalsProvider)
+    .use(MenuProvider)
+    .mount('body')
 }
-
