c60d74ebed93a18b61047a55ac62d97e193f3a16
hafri23o <natasharockbell@gmail.com>
2025-12-31 06:35:33 +0200

    Update config.tsx

--- src/pages/auth/config.tsx
+++ src/pages/auth/config.tsx
@@ -1,6 +1,7 @@
 // src/pages/auth/config.tsx
 
 // Exporting the API URLs and form fields
+export const API_URL = 'https://newmee.onrender.com/auth';
 export const LOGIN_URL = `${API_URL}/login`;
 export const SIGNUP_URL = `${API_URL}/signup`;
 
