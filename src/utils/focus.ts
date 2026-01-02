01f988e2c6068b74c3f48e17373f9fc25bff88d4
hafri23o <natasharockbell@gmail.com>
2026-01-02 18:38:02 +0200

    Update focus.ts

--- src/utils/focus.ts
+++ src/utils/focus.ts
@@ -1,27 +1,37 @@
-export const doesElementContainFocus = (element: Element): boolean =>
-  element.matches(':focus-within')
+/**
+ * Focus utilities
+ * Compatible with strict TypeScript, ESNext, and Vite
+ */
+
+export const doesElementContainFocus = (element: Element): boolean => {
+  return element.matches(':focus-within')
+}
 
 export const findFocusedElement = (
   container: Element | Document,
 ): HTMLElement | null => {
   const element = container.querySelector(':focus')
 
-  // If element contains focus it must be instanceof HTMLElement,
-  // otherwise it's always null
-  return element as HTMLElement | null
+  return element instanceof HTMLElement ? element : null
 }
 
-export const clickFocusedElement = (container: Element | Document): boolean => {
+export const clickFocusedElement = (
+  container: Element | Document,
+): boolean => {
   const element = findFocusedElement(container)
 
   if (element) {
     element.click()
     return true
   }
+
   return false
 }
 
-// https://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus/1600194#1600194
+/**
+ * Focusable elements selector
+ * Based on: https://stackoverflow.com/a/1600194
+ */
 const focusableElementQuery = `
   [tabindex]:not([tabindex='-1']),
   a[href]:not([tabindex='-1']),
@@ -31,14 +41,17 @@ const focusableElementQuery = `
   textarea:not([disabled]):not([tabindex='-1']),
   button:not([disabled]):not([tabindex='-1']),
   iframe:not([tabindex='-1']),
-  [contentEditable=true]:not([tabindex='-1'])
+  [contenteditable="true"]:not([tabindex='-1'])
 `
 
-export const isElementFocusable = (element: Element): boolean =>
-  element.matches(focusableElementQuery)
+export const isElementFocusable = (element: Element): boolean => {
+  return element.matches(focusableElementQuery)
+}
 
 export const findFocusableElement = (
   container: Element | Document,
-): HTMLElement | null =>
-  // https://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus/1600194#1600194
-  container.querySelector(focusableElementQuery)
+): HTMLElement | null => {
+  const element = container.querySelector(focusableElementQuery)
+
+  return element instanceof HTMLElement ? element : null
+}
