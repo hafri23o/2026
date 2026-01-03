dfe6e0f029fd72f40b75821d84451c079b1b3126
hafri23o <natasharockbell@gmail.com>
2026-01-03 20:43:35 +0200

    Update route.tsx

--- src/pages/not-found/route.tsx
+++ src/pages/not-found/route.tsx
@@ -1,9 +1,10 @@
 import { RouteDefinition } from 'solid-app-router'
 import { lazy } from 'solid-js'
 
+// Define a lazy-loaded route for "Not Found" page
 const route: RouteDefinition = {
-  path: '/*all',
-  component: lazy(() => import('./not-found')),
+  path: '/*all',  // This will match any route that doesn't exist
+  component: lazy(() => import('~/components/not-found')),  // Importing the NotFound component lazily
 }
 
 export default route
