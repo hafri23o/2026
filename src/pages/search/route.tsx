0c42884ba94dc30b224dd10fa55c63a4aa18cdeb
hafri23o <natasharockbell@gmail.com>
2026-01-03 21:01:10 +0200

    Update route.tsx

--- src/pages/search/route.tsx
+++ src/pages/search/route.tsx
@@ -1,10 +1,14 @@
 import { lazy } from 'solid-js'
 import { RouteDefinition } from 'solid-app-router'
 
+// Use path alias to import routes dynamically
 const route: RouteDefinition = {
   path: '/search',
-  component: lazy(() => import('./search')),
-  children: [{ path: '/' }, { path: '/:searchTerm' }],
+  component: lazy(() => import('~/pages/search')), // Use path alias to load the search component
+  children: [
+    { path: '/' },
+    { path: '/:searchTerm' },
+  ],
 }
 
 export default route
