0c1eff7646fb4d1c9538267eb18af748b16eb74c
hafri23o <natasharockbell@gmail.com>
2026-01-03 18:17:05 +0200

    Update tracks-file-parser-worker.ts

--- src/helpers/tracks-file-parser/worker/tracks-file-parser-worker.ts
+++ src/helpers/tracks-file-parser/worker/tracks-file-parser-worker.ts
@@ -1,22 +1,22 @@
-/// <reference lib='WebWorker' />
+/// <reference lib="webworker" />
 
-import { Buffer } from 'buffer'
+import { Buffer } from 'buffer'  // Ensure Buffer is properly imported for Web Worker usage
 import { parseBuffer as parseMetadata } from 'music-metadata'
 import { TrackParseMessage } from '../message-types'
 import type { UnknownTrack, FileWrapper } from '../../../types/types'
 import { extractColorFromImage } from './color-from-image'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+// Ensure globalThis.Buffer is properly set in the worker
 globalThis.Buffer = Buffer
 
-declare const self: DedicatedWorkerGlobalScope
+declare const self: DedicatedWorkerGlobalScope  // Ensure the worker scope is correctly typed
 
-// This limit is a bit arbitrary.
+// This limit is a bit arbitrary, but it's a reasonable default for large media files.
 const FILE_SIZE_LIMIT_500MB = 5e8
 
+// Function to parse a single track's metadata
 const parseTrack = async (
-  fileWrapper: FileWrapper,
+  fileWrapper: FileWrapper
 ): Promise<UnknownTrack | null> => {
   try {
     const file =
@@ -24,29 +24,35 @@ const parseTrack = async (
         ? fileWrapper.file
         : await fileWrapper.file.getFile()
 
-    // Ignore files bigger than 500mb because of
-    // potential performance issues.
+    // Ignore files larger than 500MB to avoid performance issues
     if (file.size > FILE_SIZE_LIMIT_500MB) {
       return null
     }
 
+    // Read the file into an ArrayBuffer and then into a Uint8Array
     const fileBuffer = await new Response(file).arrayBuffer()
     const fileUint8 = new Uint8Array(fileBuffer)
-    const tags = await parseMetadata(
-      fileUint8,
-      { mimeType: file.type, path: file.name, size: file.size },
-      { duration: true },
-    )
+
+    // Parse metadata using music-metadata
+    const tags = await parseMetadata(fileUint8, {
+      mimeType: file.type,
+      path: file.name,
+      size: file.size,
+    }, {
+      duration: true,
+    })
 
     const { common } = tags
 
-    let imageBlob
+    // If there is album artwork, convert it into a Blob object
+    let imageBlob: Blob | undefined
     if (common.picture?.length) {
       const [image] = common.picture
       const imageData = new Uint8ClampedArray(image.data)
       imageBlob = new Blob([imageData], { type: image.type })
     }
 
+    // Assemble the track data
     const trackData: UnknownTrack = {
       name: common.title || file.name,
       album: common.album,
@@ -61,27 +67,28 @@ const parseTrack = async (
       fileWrapper,
       primaryColor: imageBlob && (await extractColorFromImage(imageBlob)),
     }
+
     return trackData
   } catch (err) {
-    // Do not fail but still show an error.
-    // eslint-disable-next-line no-console
-    console.error(err)
-
+    // Log errors but don't let them stop the worker
+    console.error('Error parsing track:', err)
     return null
   }
 }
 
+// Function to send messages from the worker to the main thread
 const sendMsg = (options: TrackParseMessage) => {
   self.postMessage(options)
 }
 
+// Main function to parse all tracks in the provided input files
 const parseAllTracks = async (inputFiles: FileWrapper[]) => {
   let parsedCount = 0
-
   const tracks: UnknownTrack[] = []
+
+  // Iterate over all input files and parse each one
   for await (const file of inputFiles) {
     const metadata = await parseTrack(file)
-
     if (metadata) {
       parsedCount += 1
       sendMsg({ finished: false, parsedCount })
@@ -89,11 +96,14 @@ const parseAllTracks = async (inputFiles: FileWrapper[]) => {
     }
   }
 
+  // Send final message indicating that parsing is finished
   sendMsg({ finished: true, parsedCount, tracks })
-
+  
+  // Close the worker once all files are parsed
   self.close()
 }
 
+// Event listener to start parsing when the worker receives a message
 self.addEventListener('message', (event: MessageEvent<FileWrapper[]>) => {
   parseAllTracks(event.data)
 })
