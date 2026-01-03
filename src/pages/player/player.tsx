e113e1f0275d037d048b5c6b1f8b7e91a0fa0ffe
hafri23o <natasharockbell@gmail.com>
2026-01-03 20:49:31 +0200

    Update player.tsx

--- src/pages/player/player.tsx
+++ src/pages/player/player.tsx
@@ -1,10 +1,11 @@
 import { useMatch } from 'solid-app-router'
 import { createMemo, JSXElement, ParentComponent, Show } from 'solid-js'
-import { createMediaQuery } from '~/helpers/hooks/create-media-query'
-import { ControlsPane } from './controls-pane/controls-pane'
-import { QueueList } from './queue-list'
-import { Scaffold } from '~/components/scaffold/scaffold'
+import { createMediaQuery } from '~/helpers/hooks/create-media-query'  // Correct path alias
+import { ControlsPane } from './controls-pane/controls-pane'  // Correct path alias
+import { QueueList } from './queue-list'  // Correct path alias
+import { Scaffold } from '~/components/scaffold/scaffold'  // Correct path alias
 
+// A parent component for the "Now Playing" screen
 const NowPlaying: ParentComponent = (props) => (
   <Scaffold title='Now Playing' topBar={false}>
     <ControlsPane pinned={Boolean(props.children)} />
@@ -12,6 +13,7 @@ const NowPlaying: ParentComponent = (props) => (
   </Scaffold>
 )
 
+// Component to handle the separate pages (full player and queue list)
 const FullPlayerSeparatePages = () => {
   const mainPlayerMatch = useMatch(() => '/player')
   const isMainPlayer = createMemo(() => Boolean(mainPlayerMatch()))
@@ -25,6 +27,7 @@ const FullPlayerSeparatePages = () => {
   )
 }
 
+// Main Full Player component, using media query for responsive design
 const FullPlayer = (): JSXElement => {
   const isCompact = createMediaQuery('(max-width: 700px), (max-height: 640px)')
 
