3782ba17bcce8fd90b9a7a3a3ef25b5e19aa6249
hafri23o <natasharockbell@gmail.com>
2026-01-03 20:26:01 +0200

    Update error.tsx

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,17 +1,20 @@
 import { ParentComponent, ErrorBoundary } from 'solid-js'
 
+// Fallback component for handling errors
 const Fallback = (error: Error) => (
-  <div style='user-select: text;'>
-    <div>Unknown error has occured please</div>
-    <a href='/'>Reload</a>
+  <div style="user-select: text;">
+    <div>Unknown error has occurred, please</div>
+    <a href="/">Reload</a>
     <details>
       <summary>Internal Error logs</summary>
-      {console.log(error) as unknown as string}
+      {/* Log error information for debugging */}
+      {console.log(error)}
       <p>{error.stack?.toString()}</p>
     </details>
   </div>
 )
 
+// The ErrorPage component, wrapping children with the ErrorBoundary
 export const ErrorPage: ParentComponent = (props) => (
   <ErrorBoundary fallback={Fallback}>{props.children}</ErrorBoundary>
 )
