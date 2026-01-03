08db51950c87eab4305e326a328a9516bee8738f
hafri23o <natasharockbell@gmail.com>
2026-01-03 22:23:10 +0200

    Update list.css.ts

--- src/components/list/list.css.ts
+++ src/components/list/list.css.ts
@@ -4,4 +4,6 @@ export const listContainer = style({
   display: 'flex',
   flexDirection: 'column',
   outline: 'none',
+  // Add some margin for spacing in the container, useful for layout
+  margin: 0, 
 })
