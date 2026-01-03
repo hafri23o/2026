464a3a82c9c75fc3eb33190f6c51abb5708fee84
hafri23o <natasharockbell@gmail.com>
2026-01-03 22:19:24 +0200

    Update icon.tsx

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,17 +1,17 @@
-import { VoidComponent } from 'solid-js'
-import { clx } from '~/utils'
-import { ICON_PATHS } from './icon-paths'
-import * as styles from './icon.css'
+import { VoidComponent } from 'solid-js';
+import { clx } from '~/utils'; // Ensure `clx` is defined in ~/utils.ts or ~/utils/index.ts
+import { ICON_PATHS } from './icon-paths'; // Make sure ICON_PATHS is correctly imported
+import * as styles from './icon.css'; // CSS module import for styles
 
-export type IconType = keyof typeof ICON_PATHS
+export type IconType = keyof typeof ICON_PATHS;
 
 export interface IconProps {
-  icon: IconType
-  class?: string
+  icon: IconType;
+  class?: string;
 }
 
 export const Icon: VoidComponent<IconProps> = (props) => (
-  <svg viewBox='0 0 24 24' class={clx(styles.icon, props.class)}>
+  <svg viewBox="0 0 24 24" class={clx(styles.icon, props.class)}>
     <path d={ICON_PATHS[props.icon]} />
   </svg>
-)
+);
