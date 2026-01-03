a215282660546c3a0136409c0e0d0b676ca190d0
hafri23o <natasharockbell@gmail.com>
2026-01-03 18:15:07 +0200

    Update color-from-image.ts

--- src/helpers/tracks-file-parser/worker/color-from-image.ts
+++ src/helpers/tracks-file-parser/worker/color-from-image.ts
@@ -4,6 +4,11 @@ import {
   Score,
 } from '@material/material-color-utilities'
 
+/**
+ * Fetches image data from a given Blob (image).
+ * @param blob The Blob object containing image data.
+ * @returns Uint8ClampedArray containing pixel data.
+ */
 const getImageData = async (blob: Blob): Promise<Uint8ClampedArray> => {
   const bitmap = await createImageBitmap(blob)
 
@@ -12,24 +17,31 @@ const getImageData = async (blob: Blob): Promise<Uint8ClampedArray> => {
   const canvas = new OffscreenCanvas(width, height)
   const ctx = canvas.getContext('2d')
   if (!ctx) {
-    throw new Error()
+    throw new Error('Failed to get 2D context from canvas.')
   }
 
   ctx.drawImage(bitmap, 0, 0)
   return ctx.getImageData(0, 0, width, height).data
 }
 
+/**
+ * Extracts a color from an image Blob, specifically the dominant color.
+ * @param blob The Blob object containing image data.
+ * @returns The dominant color as a 32-bit ARGB integer, or undefined if it fails.
+ */
 export const extractColorFromImage = async (
-  blob: Blob,
+  blob: Blob
 ): Promise<number | undefined> => {
-  // Firefox 91 and Safari doesn't support it yet.
-  if (globalThis.OffscreenCanvas === undefined) {
+  // Check for OffscreenCanvas support in the current environment.
+  if (typeof OffscreenCanvas === 'undefined') {
+    console.warn('OffscreenCanvas is not supported in this environment.')
     return undefined
   }
 
   try {
     const imageBytes = await getImageData(blob)
 
+    // Convert the image data to ARGB format
     const pixels: number[] = []
     for (let i = 0; i < imageBytes.length; i += 4) {
       const r = imageBytes[i]
@@ -42,14 +54,14 @@ export const extractColorFromImage = async (
       }
     }
 
-    // Convert Pixels to Material Colors
+    // Quantize the pixel data and find the dominant color using the Material Color Utilities.
     const result = QuantizerCelebi.quantize(pixels, 128)
     const ranked = Score.score(result)
     const top = ranked[0]
 
     return top
   } catch (err) {
-    console.error(err)
+    console.error('Error extracting color from image:', err)
   }
 
   return undefined
