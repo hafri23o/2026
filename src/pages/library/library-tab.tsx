303f534889e390c8cb0c843bb45014915ec44619
hafri23o <natasharockbell@gmail.com>
2026-01-03 20:28:32 +0200

    Update library-tab.tsx

--- src/pages/library/library-tab.tsx
+++ src/pages/library/library-tab.tsx
@@ -1,19 +1,21 @@
-import { createMemo, VoidComponent } from 'solid-js'
-import { Dynamic } from 'solid-js/web'
-import { ScrollContainer } from '../../components/scroll-container/scroll-container'
+import { createMemo, VoidComponent } from 'solid-js'  // Import Solid.js hooks
+import { Dynamic } from 'solid-js/web'  // Dynamic component for conditional rendering
+import { ScrollContainer } from '~/components/scroll-container/scroll-container'  // Using the '~' alias
 import {
   useEntitiesStore,
   useLibraryStore
-} from '../../stores/stores'
-import { BaseMusicItem, MusicItemType, Track } from '../../types/types'
-import { sortByKey } from '../../utils'
-import { LibraryPageConfig } from './config'
-import * as styles from './library.css'
+} from '~/stores/stores'  // Using the '~' alias for stores
+import { BaseMusicItem, MusicItemType, Track } from '~/types/types'  // Using the '~' alias for types
+import { sortByKey } from '~/utils'  // Using the '~' alias for utils
+import { LibraryPageConfig } from './config'  // Correct path for configuration
+import * as styles from './library.css'  // Using local styles for CSS Modules
 
 export const LibraryPage: VoidComponent<LibraryPageConfig> = (props) => {
+  // Access store states
   const [dataState] = useEntitiesStore()
   const [libraryState] = useLibraryStore()
 
+  // Function to select items based on type
   const itemsSelector = () => {
     switch (props.type) {
       case MusicItemType.ALBUM:
@@ -25,23 +27,25 @@ export const LibraryPage: VoidComponent<LibraryPageConfig> = (props) => {
       case MusicItemType.PLAYLIST:
         return dataState.playlists
       default:
-        throw new Error('Wrong item type')
+        throw new Error('Wrong item type')  // Handle invalid item type
     }
   }
 
+  // Memoized item IDs sorted by the selected key
   const itemIds = createMemo(() => {
     const itemsArray = sortByKey(
-      [...Object.values<Track>(itemsSelector() as { [key: string]: Track })],
-      libraryState.sortKeys[props.type] as keyof Track,
-    ) as unknown as BaseMusicItem[]
+      [...Object.values<Track>(itemsSelector() as { [key: string]: Track })], 
+      libraryState.sortKeys[props.type] as keyof Track  // Sorting by key from state
+    ) as unknown as BaseMusicItem[]  // Casting to the correct type
 
-    return itemsArray.map((item) => item.id)
+    return itemsArray.map((item) => item.id)  // Return sorted item IDs
   })
 
+  // Return JSX structure for the library page
   return (
     <ScrollContainer class={styles.libraryPageContainer} observeScrollState>
-      {props.actions && <div class={styles.actions}>{props.actions}</div>}
-      <Dynamic component={props.component} items={itemIds()} />
+      {props.actions && <div class={styles.actions}>{props.actions}</div>}  // Display actions if provided
+      <Dynamic component={props.component} items={itemIds()} />  // Dynamically render the component with item IDs
     </ScrollContainer>
   )
 }
