e3d8b2c622742b0bfc52949b0976c06914ed76fe
hafri23o <natasharockbell@gmail.com>
2026-01-03 18:55:42 +0200

    Update routes.tsx

--- src/pages/app/routes.tsx
+++ src/pages/app/routes.tsx
@@ -2,13 +2,13 @@ import { RouteDefinition, Navigate } from 'solid-app-router'
 import libraryRoute, {
   LIBRARY_PATH,
   DEFAULT_LIBRARY_PATH,
-} from '../library/route'
-import playerRoute from '../player/route'
-import detailsRoutes from '../details/route'
-import searchRoute from '../search/route'
-import settingsRoute from '../settings/route'
-import aboutRoute from '../about/route'
-import notFoundRoute from '../not-found/route'
+} from '~/library/route'  // Adjusting path to use the alias '~'
+import playerRoute from '~/player/route'  // Adjusting path to use the alias '~'
+import detailsRoutes from '~/details/route'  // Adjusting path to use the alias '~'
+import searchRoute from '~/search/route'  // Adjusting path to use the alias '~'
+import settingsRoute from '~/settings/route'  // Adjusting path to use the alias '~'
+import aboutRoute from '~/about/route'  // Adjusting path to use the alias '~'
+import notFoundRoute from '~/not-found/route'  // Adjusting path to use the alias '~'
 
 export const ROUTES: RouteDefinition[] = [
   libraryRoute,
