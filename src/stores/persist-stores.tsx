dc39798c623ed6711da9b1168774660c08ccd0a2
hafri23o <natasharockbell@gmail.com>
2026-01-05 19:15:12 +0200

    Update persist-stores.tsx

--- src/stores/persist-stores.tsx
+++ src/stores/persist-stores.tsx
@@ -5,7 +5,6 @@ import {
   on,
   Show,
   ParentComponent,
-  untrack,
 } from 'solid-js'
 import { unwrap } from 'solid-js/store'
 import {
@@ -15,8 +14,9 @@ import {
 } from 'idb-keyval'
 import { deleteIDBDatabases } from '../helpers/delete-idb-databases'
 
-// Interface for a persisted item with a generic value type
-interface PersistedItem<V = unknown> {
+// TODO. Generics arrays with mixed types are hard, fix this one day. Maybe.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+interface PersistedItem<V = any> {
   key: string
   selector: () => V
   load: (value: V) => void
@@ -40,17 +40,16 @@ export const PersistStoresProvider: ParentComponent<PersistStoresProps> = (
     .flat(1)
 
   const { storageName, version } = props
+
   const [isLoaded, setIsLoaded] = createSignal(false)
 
   const fullName = `${storageName}-${version}`
-  const storeIDB = createStoreIDB(fullName, fullName)
 
-  // Delete previous IDB versions for cleanup
+  const storeIDB = createStoreIDB(fullName, fullName)
   deleteIDBDatabases(storageName, version)
 
   const persistedKeys = persistedItems.map((t) => t.key)
 
-  // Load persisted items from IndexedDB initially
   const setInitiallyLoadedState = (values: unknown[]) => {
     values.forEach((value, index) => {
       if (value !== undefined) {
@@ -59,44 +58,41 @@ export const PersistStoresProvider: ParentComponent<PersistStoresProps> = (
     })
   }
 
-  // Asynchronously load persisted values from IndexedDB
   getManyIDB(persistedKeys, storeIDB)
     .then(setInitiallyLoadedState)
     .finally(() => setIsLoaded(true))
 
   createEffect(
     on(isLoaded, (loaded) => {
-      if (!loaded) return
+      if (!loaded) {
+        return
+      }
 
+      // Don't save data on initial load.
       let skippedSetup = false
-
-      // Listen for changes in persisted items and save them to IDB
+      // Listen for changes to values and save them to IDB.
       persistedItems.forEach((item) => {
         createEffect(() => {
           const value: unknown = item.selector()
 
-          // Track changes for complex objects to ensure reactivity
           if (typeof value === 'object' && value !== null) {
             trackStore(item.selector())
           }
 
-          // Save changes to IndexedDB after initial setup
           if (skippedSetup) {
             setIDB(item.key, unwrap(value), storeIDB)
           }
         })
       })
 
-      // Ensure setup is skipped on the first render
+      // createEffect doesn't fire synchronously
       createEffect(() => {
         skippedSetup = true
       })
 
-      // Callback after loading the persisted data
       props.onLoad?.()
     }),
   )
 
-  // Only render children when data is fully loaded from IndexedDB
   return <Show when={isLoaded()}>{props.children}</Show>
 }
