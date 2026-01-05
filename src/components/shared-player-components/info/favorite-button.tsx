a0e5b10f55bdc7ceca469b9c11a2e23351408548
hafri23o <natasharockbell@gmail.com>
2026-01-05 16:44:22 +0200

    Update favorite-button.tsx

--- src/components/shared-player-components/info/favorite-button.tsx
+++ src/components/shared-player-components/info/favorite-button.tsx
@@ -4,21 +4,24 @@ import { useEntitiesStore, usePlayerStore } from '~/stores/stores'
 import * as styles from './info.css'
 
 export const FavoriteButton = (): JSXElement => {
+  // Accessing store states
   const [dataState, dataActions] = useEntitiesStore()
   const [playerState] = usePlayerStore()
 
+  // Memoized value to get the active track
   const aTrack = () => playerState.activeTrack
 
-  const isFavorited = createMemo(() =>
-    dataState.favorites.includes(playerState.activeTrackId),
+  // Memoized computation to check if the track is favorited
+  const isFavorited = createMemo(() => 
+    dataState.favorites.includes(playerState.activeTrackId)
   )
 
+  // Handler for the favorite button click
   const onFavoriteBtnClickHandler = () => {
     const trackId = aTrack()?.id
-    if (!trackId) {
-      return
-    }
+    if (!trackId) return
 
+    // If the track is already favorited, unfavorite it; otherwise, favorite it
     if (isFavorited()) {
       dataActions.unfavoriteTrack(trackId)
     } else {
@@ -28,11 +31,11 @@ export const FavoriteButton = (): JSXElement => {
 
   return (
     <IconButton
-      class={styles.infoFavoriteBtn}
+      class={styles.infoFavoriteBtn} // applying styles via vanilla-extract
       title={isFavorited() ? 'Remove from Favorites' : 'Add to Favorites'}
       icon={isFavorited() ? 'favorite' : 'favoriteOutline'}
       onClick={onFavoriteBtnClickHandler}
-      disabled={!aTrack()}
+      disabled={!aTrack()} // Disable if no active track
     />
   )
 }
