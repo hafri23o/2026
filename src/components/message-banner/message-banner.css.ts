f96d5f173950d14dbf64ffcc19619619701e0341
hafri23o <natasharockbell@gmail.com>
2026-01-05 15:59:21 +0200

    Update message-banner.css.ts

--- src/components/message-banner/message-banner.css.ts
+++ src/components/message-banner/message-banner.css.ts
@@ -1,13 +1,16 @@
 import { style } from '@vanilla-extract/css'
 import { sharedStyles, sprinkles } from '~/styles/styles.css'
 
+// Extracting the outlinedButton from sharedStyles for reusability
 export const { outlinedButton } = sharedStyles
 
+// Define the title style using sprinkles
 export const title = sprinkles({
   typography: 'headlineSmall',
   color: 'onSurface',
 })
 
+// Define the message banner styles, combining sprinkles and custom styles
 export const messageBanner = style([
   sprinkles({
     typography: 'titleMedium',
@@ -19,7 +22,7 @@ export const messageBanner = style([
     alignItems: 'center',
   }),
   style({
-    textAlign: 'center',
-    margin: 'auto',
+    textAlign: 'center', // Center-align the text
+    margin: 'auto', // Auto margin for centering the element
   }),
 ])
