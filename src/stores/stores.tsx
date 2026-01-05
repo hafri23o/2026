dde0e9ab46803ecebaba923dc41a4fc7f107d650
hafri23o <natasharockbell@gmail.com>
2026-01-05 19:15:44 +0200

    Update stores.tsx

--- src/stores/stores.tsx
+++ src/stores/stores.tsx
@@ -1,87 +1,52 @@
 import { createContext, ParentComponent, useContext } from 'solid-js'
+import { createEntitiesStore } from './entities/create-entities-store'
+import { createLibraryStore } from './library/create-library-store'
+import { createPlayerStore } from './player/create-player-store'
+import { PersistStoresProvider, PersistStoresProps } from './persist-stores'
 
-import { createEntitiesStore } from '~/stores/entities/create-entities-store'
-import { createLibraryStore } from '~/stores/library/create-library-store'
-import { createPlayerStore } from '~/stores/player/create-player-store'
-import {
-  PersistStoresProvider,
-  PersistStoresProps,
-} from '~/stores/persist-stores'
-
-/**
- * Generic helper to create a strongly typed Solid context store
- * compatible with strict TypeScript settings.
- */
 function createStoreCtx<T>(createStoreFn: () => T) {
-  const StoreContext = createContext<T | undefined>(undefined)
+  const StoreContext = createContext<T>()
 
   const Provider: ParentComponent = (props) => {
-    const store = createStoreFn()
-
+    const state = createStoreFn()
     return (
-      <StoreContext.Provider value={store}>
+      <StoreContext.Provider value={state}>
         {props.children}
       </StoreContext.Provider>
     )
   }
 
-  const useStore = (): T => {
-    const ctx = useContext(StoreContext)
-    if (!ctx) {
-      throw new Error(
-        'Store context was used outside of its corresponding Provider',
-      )
-    }
-    return ctx
-  }
+  const useStateContext = () => useContext(StoreContext) as T
 
-  return [Provider, useStore] as const
+  return [Provider, useStateContext] as const
 }
 
-/* ------------------------------------------------------------------ */
-/* Stores                                                             */
-/* ------------------------------------------------------------------ */
-
-const [EntitiesProvider, useEntitiesStore] =
-  createStoreCtx(createEntitiesStore)
-
-const [LibraryProvider, useLibraryStore] =
-  createStoreCtx(createLibraryStore)
-
-const [PlayerProvider, usePlayerStore] =
-  createStoreCtx(createPlayerStore)
+const [EntitiesProvider, useEntitiesStore] = createStoreCtx(createEntitiesStore)
+const [LibraryProvider, useLibraryStore] = createStoreCtx(createLibraryStore)
+const [PlayerProvider, usePlayerStore] = createStoreCtx(createPlayerStore)
 
 export { useEntitiesStore, useLibraryStore, usePlayerStore }
 
-/* ------------------------------------------------------------------ */
-/* Persistence setup                                                   */
-/* ------------------------------------------------------------------ */
-
-// ⚠️ Only change when persisted data structure changes
+// Only change version if there are breaking changes
+// with persisted data. Changing version number
+// will delete all previous storages.
 const APP_STORAGE_VERSION = 1
 const APP_STORAGE_NAME = 'APP_DATA'
 
 export const RootStoresProvider: ParentComponent<
   Pick<PersistStoresProps, 'onLoad'>
-> = (props) => {
-  return (
-    <EntitiesProvider>
-      <LibraryProvider>
-        <PlayerProvider>
-          <PersistStoresProvider
-            storageName={APP_STORAGE_NAME}
-            version={APP_STORAGE_VERSION}
-            useStores={[
-              useEntitiesStore,
-              useLibraryStore,
-              usePlayerStore,
-            ]}
-            onLoad={props.onLoad}
-          >
-            {props.children}
-          </PersistStoresProvider>
-        </PlayerProvider>
-      </LibraryProvider>
-    </EntitiesProvider>
-  )
-}
+> = (props) => (
+  <EntitiesProvider>
+    <LibraryProvider>
+      <PlayerProvider>
+        <PersistStoresProvider
+          storageName={APP_STORAGE_NAME}
+          version={APP_STORAGE_VERSION}
+          useStores={[useEntitiesStore, useLibraryStore, usePlayerStore]}
+        >
+          {props.children}
+        </PersistStoresProvider>
+      </PlayerProvider>
+    </LibraryProvider>
+  </EntitiesProvider>
+)
