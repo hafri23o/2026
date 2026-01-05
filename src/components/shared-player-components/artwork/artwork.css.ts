22f55cc25cbe25efe92ea31c9be6239add788990
hafri23o <natasharockbell@gmail.com>
2026-01-05 16:34:24 +0200

    Update artwork.css.ts

--- src/components/shared-player-components/artwork/artwork.css.ts
+++ src/components/shared-player-components/artwork/artwork.css.ts
@@ -1,5 +1,8 @@
 import { style } from '@vanilla-extract/css'
 
+// Define styles for the artwork
 export const artwork = style({
-  transition: 'background-image .2s',
+  transition: 'background-image 0.2s ease-in-out', // Adding a smoother transition
+  backgroundPosition: 'center',  // Center the background
+  backgroundSize: 'cover',      // Ensure the background image covers the element's area
 })
