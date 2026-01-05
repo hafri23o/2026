c0fbaa6e4216db36dde67b872269aa0536532604
hafri23o <natasharockbell@gmail.com>
2026-01-05 16:56:20 +0200

    Update slider.css.ts

--- src/components/slider/slider.css.ts
+++ src/components/slider/slider.css.ts
@@ -2,6 +2,7 @@ import { style, createVar, fallbackVar } from '@vanilla-extract/css'
 import { getVarName } from '../../styles/css-helpers'
 import { vars } from '../../styles/styles.css'
 
+// Create the CSS variables for dynamic slider values and colors
 export const sliderValueVar = createVar()
 export const sliderValueVarName = getVarName(sliderValueVar)
 
@@ -11,9 +12,11 @@ const appearance = {
   MozAppearance: 'none',
 } as const
 
+// Define CSS variables for slider colors
 export const sliderTrackColorVar = createVar()
 export const sliderProgressColorVar = createVar()
 
+// Styling for the slider thumb (the draggable part of the slider)
 const sliderThumb = {
   ...appearance,
   background: 'currentcolor',
@@ -24,13 +27,15 @@ const sliderThumb = {
   boxShadow: 'none',
 }
 
+// Disabled state for the slider (to hide the thumb when disabled)
 const disabled = {
   display: 'none',
-  // Firefox
+  // Firefox-specific handling
   width: 0,
   height: 0,
 }
 
+// Main slider track style
 export const slider = style({
   ...appearance,
   cursor: 'pointer',
@@ -43,10 +48,12 @@ export const slider = style({
   background: fallbackVar(sliderTrackColorVar, vars.colors.outline),
   color: fallbackVar(sliderProgressColorVar, vars.colors.primary),
   selectors: {
+    // Styling for the slider progress in Firefox
     '&::-moz-range-progress': {
       ...appearance,
       background: 'currentColor',
     },
+    // Styling for the slider in Webkit browsers (Chrome, Safari, etc.)
     '&::-webkit-slider-container': {
       ...appearance,
       borderRadius: 'inherit',
@@ -56,10 +63,13 @@ export const slider = style({
         transparent 0%
       )`,
     },
+    // Styling for the slider thumb (the draggable part)
     '&::-webkit-slider-thumb': sliderThumb,
     '&::-moz-range-thumb': sliderThumb,
+    // Disabled state for the slider thumb in Webkit browsers
     '&:disabled::-webkit-slider-thumb': disabled,
     '&:disabled::-moz-range-thumb': disabled,
+    // Focus state for the slider (to improve accessibility)
     '&:focus-visible': {
       outline: '2px solid currentColor',
       outlineOffset: '6px',
