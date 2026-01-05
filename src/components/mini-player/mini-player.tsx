7139f60f7220496ef51d3838646bda7cbfadaca6
hafri23o <natasharockbell@gmail.com>
2026-01-05 16:03:24 +0200

    Update mini-player.tsx

--- src/components/mini-player/mini-player.tsx
+++ src/components/mini-player/mini-player.tsx
@@ -1,25 +1,25 @@
 import { useNavigate } from 'solid-app-router'
 import { Show, VoidComponent } from 'solid-js'
-import { createMediaQuery } from '~/helpers/hooks/create-media-query'
-import { Artwork } from '../shared-player-components/artwork/artwork'
+import { createMediaQuery } from '~/helpers/hooks/create-media-query' // Correct path with alias
+import { Artwork } from '../shared-player-components/artwork/artwork' // Assuming components exist under this path
 import { Controls } from '../shared-player-components/controls/main-controls'
 import { PlayNextButton } from '../shared-player-components/controls/play-next-prev-buttons'
 import { PlayPauseButton } from '../shared-player-components/controls/play-pause-button'
 import { VolumePanel } from '../shared-player-components/volume/volume-panel'
 import { Info } from '../shared-player-components/info/info'
 import { Timeline } from '../shared-player-components/timeline/timeline'
-import { clx } from '~/utils'
+import { clx } from '~/utils' // Ensure utility functions are also imported using alias
 import { FavoriteButton } from '../shared-player-components/info/favorite-button'
-import { Icon } from '../icon/icon'
-import * as styles from './mini-player.css'
+import { Icon } from '../icon/icon' // Assuming this is another component
+import * as styles from './mini-player.css' // Assuming CSS is written using @vanilla-extract
 
 export interface MiniPlayerProps {
   class?: string | false
 }
 
 export const MiniPlayer: VoidComponent<MiniPlayerProps> = (props) => {
-  const areMainControlsHidden = createMediaQuery(styles.COMPACT_MEDIA)
-
+  // Create media queries based on screen size
+  const areMainControlsHidden = createMediaQuery(styles.COMPACT_MEDIA) // Using compact media query defined in CSS
   const areSecondaryActionsVisible = createMediaQuery('(min-width: 421px)')
   const isPlayPauseButtonVisible = createMediaQuery('(min-width: 300px)')
 
@@ -27,34 +27,38 @@ export const MiniPlayer: VoidComponent<MiniPlayerProps> = (props) => {
 
   return (
     <div class={clx(styles.container, props.class)}>
+      {/* Show Timeline only when main controls are not hidden */}
       {!areMainControlsHidden() && <Timeline />}
 
       <div class={styles.infoSection}>
         <button
           title='Open player'
           class={styles.openFullPlayerButton}
-          onClick={() => navigate('/player')}
+          onClick={() => navigate('/player')} // Navigate to player page
         >
           <div class={styles.artworkContainer}>
-            <Icon icon='chevronUp' class={styles.artworkArrow} />
-            <Artwork />
+            <Icon icon='chevronUp' class={styles.artworkArrow} /> {/* Custom Icon */}
+            <Artwork /> {/* Artwork component */}
           </div>
-          <Info />
+          <Info /> {/* Info section */}
         </button>
 
+        {/* Show FavoriteButton only when secondary actions are visible */}
         {areSecondaryActionsVisible() && <FavoriteButton />}
       </div>
+
+      {/* Conditionally render controls and play/pause button */}
       <Show
-        when={!areMainControlsHidden()}
+        when={!areMainControlsHidden()} 
         fallback={
           <>
-            {isPlayPauseButtonVisible() && <PlayPauseButton />}
-            {areSecondaryActionsVisible() && <PlayNextButton />}
+            {isPlayPauseButtonVisible() && <PlayPauseButton />} {/* Play/Pause button */}
+            {areSecondaryActionsVisible() && <PlayNextButton />} {/* Next/Previous buttons */}
           </>
         }
       >
-        <Controls />
-        <VolumePanel />
+        <Controls /> {/* Main controls */}
+        <VolumePanel /> {/* Volume control */}
       </Show>
     </div>
   )
