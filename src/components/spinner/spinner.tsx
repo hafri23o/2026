1316102b54e80a4416ce3c3ead92577ccc747435
hafri23o <natasharockbell@gmail.com>
2026-01-05 17:00:44 +0200

    Update spinner.tsx

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,13 +1,21 @@
 import { VoidComponent } from 'solid-js'
-import { clx } from '../../utils'
+import { clx } from '../../utils'  // Assuming this is a utility for className merging
 import * as styles from './spinner.css'
 
+// SpinnerProps interface remains the same for optional className
 export interface SpinnerProps {
   class?: string
 }
 
-export const Spinner: VoidComponent<SpinnerProps> = (props) => (
-  <svg class={clx(styles.spinner, props.class)} viewBox='0 0 66 66'>
-    <circle class={styles.path} cx='33' cy='33' r='30' />
-  </svg>
-)
+// Functional component for the Spinner
+export const Spinner: VoidComponent<SpinnerProps> = (props) => {
+  return (
+    <svg
+      class={clx(styles.spinner, props.class)} // Combine spinner styles with any passed className
+      viewBox='0 0 66 66'
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle class={styles.path} cx="33" cy="33" r="30" />
+    </svg>
+  )
+}
